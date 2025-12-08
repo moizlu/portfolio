586aa159d850b5f61d929c9f108fcec09587fd98
もいずる <momizibe@outlook.jp>
2025-12-08 10:40:43 +0900

    Update Home.svelte

--- src/lib/components/sections/pages/Home/Home.svelte
+++ src/lib/components/sections/pages/Home/Home.svelte
@@ -2,7 +2,7 @@
     import logo from "$lib/assets/logo.svg";
 </script>
 
-<section id="home" class="min-h-screen max-md:pb-30 scroll-mb-[70px] md:scroll-mt-[70px] pt-3 flex flex-col justify-center items-center gap-2">
+<section id="home" class="min-h-screen max-md:pb-40 scroll-mb-[70px] md:scroll-mt-[70px] pt-3 flex flex-col justify-center items-center gap-2">
     <img src={logo} alt="logo" width={300} height={300} class="drop-shadow-black drop-shadow-md/50">
 
     <p class="text-6xl">もいずる</p>
