fb20653d79491062d394a519015106d9b9b3c18f
もいずる <momizibe@outlook.jp>
2026-01-22 20:26:22 +0900

    Update Header.svelte

--- src/lib/components/sections/Header/Header.svelte
+++ src/lib/components/sections/Header/Header.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <a href="#home" class={["z-1000 fixed top-0 left-0 m-5 md:m-1", (dialog.getContent()) && "pointer-events-none"]}>
-    <img src={logo} alt="logo" class="pointer-events-none w-10 sm:w-15 h-full drop-shadow-black drop-shadow-sm/100">
+    <img src={logo} alt="logo" class="pointer-events-none w-10 sm:w-15 h-full opacity-50 sm:opacity-100 drop-shadow-black drop-shadow-sm/100">
 </a>
 
 <header class="z-10 fixed bottom-0 md:top-0 left-0 w-full h-[90px] md:h-[70px] max-md:pb-4 p-2 flex justify-start md:justify-end items-center backdrop-blur-2xl">
