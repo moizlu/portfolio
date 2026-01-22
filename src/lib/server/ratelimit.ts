21af5ad406ebde1228aceb9c2a0a61927c93b779
もいずる <momizibe@outlook.jp>
2026-01-22 22:52:51 +0900

    Update ratelimit.ts

--- src/lib/server/ratelimit.ts
+++ src/lib/server/ratelimit.ts
@@ -1,8 +1,14 @@
 import { Redis } from '@upstash/redis';
 import { Ratelimit } from '@upstash/ratelimit';
+import { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } from '$env/static/private';
+
+const redis = new Redis({
+  url: UPSTASH_REDIS_REST_URL,
+  token: UPSTASH_REDIS_REST_TOKEN,
+});
 
 export const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
+  redis: redis,
   limiter: Ratelimit.slidingWindow(5, '24 h'),
   analytics: true
 });
