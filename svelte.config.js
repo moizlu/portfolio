1d834aeea7e14a22a1c07c8a46af67a2fb15911e
もいずる <momizibe@outlook.jp>
2026-01-23 11:36:07 +0900

    CSP制限の追加

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,21 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+
+		csp: {
+			mode: "nonce",
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self', "https://challenges.cloudflare.com https://static.cloudflareinsights.com", 'unsafe-inline'],
+				'style-src': ['self', "https://fonts.googleapis.com", 'unsafe-inline'],
+				'font-src': ['self', "https://fonts.gstatic.com"],
+				'frame-src': ['self', "https://challenges.cloudflare.com"],
+				'img-src': ['self', 'data:', 'https:']
+			}
+		}
+	}
 };
 
 export default config;
