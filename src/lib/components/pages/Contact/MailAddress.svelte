fb296c5496309a179ac46cbd964ffcee021ef66e
もいずる <momizibe@outlook.jp>
2026-02-10 18:28:46 +0900

    Update MailAddress.svelte

--- src/lib/components/pages/Contact/MailAddress.svelte
+++ src/lib/components/pages/Contact/MailAddress.svelte
@@ -3,6 +3,8 @@
     import JumpIcon from "$lib/assets/icons/jump.svelte";
     import CopyIcon from "$lib/assets/icons/copy.svelte";
 
+    import { turnstileState } from "$lib/state/state.svelte";
+
     import SvgIcon from "$lib/components/ui/SvgIcon";
 
     let addr: string | null = $state(null);
@@ -13,7 +15,7 @@
     }
 
     const onDisplayMailButtonClick = () => {
-        if (!mailAddrElement) { return; }
+        if (!mailAddrElement || !turnstileState.isVerified) { return; }
 
         if (mailAddrElement.textContent === addr) {
             const a = document.createElement('a');
@@ -26,7 +28,7 @@
     }
 
     const onMailCopyButtonClick = () => {
-        if (!mailAddrElement) { return; }
+        if (!mailAddrElement || !turnstileState.isVerified) { return; }
 
         if (!addr) {
             addr = genAddr();
@@ -39,7 +41,7 @@
 </script>
 
 <div class="m-2 flex-center gap-2">
-    <button onclick={onDisplayMailButtonClick} class="w-52 p-1 flex justify-start items-center button-general cursor-pointer">
+    <button onclick={onDisplayMailButtonClick} disabled={!turnstileState.isVerified} class="w-52 p-1 flex justify-start items-center button-general cursor-pointer">
         {#if addr}
             <SvgIcon Svg={JumpIcon} size={30} class="" />
         {:else}
@@ -47,10 +49,10 @@
         {/if}
         <p bind:this={mailAddrElement}>メールアドレスを表示</p>
     </button>
-    <button onclick={onMailCopyButtonClick} title="copy email" class="p-1 flex-center button-general cursor-pointer">
+    <button onclick={onMailCopyButtonClick} title="copy email" disabled={!turnstileState.isVerified} class="p-1 flex-center button-general cursor-pointer">
         <SvgIcon Svg={CopyIcon} size={30} />
         <p>コピー</p>
     </button>
 </div>
 
-<p class="m-1 text-xs text-center">添付ファイルがある場合やフォームが送信できない時は、<wbr>こちらに直接メールをお送りください。</p>
+<p class="m-1 text-xs text-center">添付ファイルがある場合やフォームが送信できない時は、<wbr>こちらに直接メールをお送りください。<br><span class={["text-sm", (turnstileState.isVerified) && "invisible"]}>(Bot認証を通過すると使用可能になります。)</span></p>
