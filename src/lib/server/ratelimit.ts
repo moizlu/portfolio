a9a1501428dba7ddd86d2d64ba20d2c6d0a07955
もいずる <momizibe@outlook.jp>
2026-02-21 17:05:22 +0900

    Update ratelimit.ts

--- src/lib/server/ratelimit.ts
+++ src/lib/server/ratelimit.ts
@@ -1,4 +1,4 @@
-import { Redis } from '@upstash/redis';
+import { Redis } from '@upstash/redis/cloudflare';
 import { Ratelimit } from '@upstash/ratelimit';
 import { env } from '$env/dynamic/private';
 
