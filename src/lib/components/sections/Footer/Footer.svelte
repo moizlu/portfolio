3a31355fedce833400b25a102565cc46ea997f03
もいずる <momizibe@outlook.jp>
2026-03-05 14:33:57 +0900

    フッターのデザインを調整

--- src/lib/components/sections/Footer/Footer.svelte
+++ src/lib/components/sections/Footer/Footer.svelte
@@ -21,35 +21,38 @@
     let prevSection = $derived(sectionIndexes[sectionState.getIndex(sectionState.activeSection) + 1] ?? sectionIndexes.at(-1));
 </script>
 
-<footer class="transition-all duration-300 fixed bottom-25 md:bottom-2 left-0 px-4 w-full h-17 pointer-events-none">
+<footer class="transition-all duration-300 fixed bottom-25 md:bottom-2 left-0 w-full h-15 pointer-events-none">
     {#if browser && window.location.pathname === '/'}
-        <div class="w-full mb-2 flex justify-between items-center">
-            <div class="flex-center gap-2 pointer-events-auto">
-                <a href="https://moiz.lu/github" target="_blank" title="github" class="p-2 rounded-full bg-base/25 backdrop-blur-2xl pointer-events-auto">
+        <div class="w-full h-13 pl-2 mb-2 flex justify-between items-center">
+            <div class="max-[329px]:scale-75 max-[329px]:-translate-x-3 min-[1027px]:w-90 flex-center gap-2 pointer-events-auto border-label">
+                <a href="https://moiz.lu/github" target="_blank" title="github" class="p-2 min-[1027px]:flex-1 rounded-full bg-base/25 backdrop-blur-2xl border flex justify-start items-center gap-2 pointer-events-auto">
                     <Icon lightSrc={GitHubLightIcon} darkSrc={GitHubDarkIcon} size={30} />
+                    <p class="hidden min-[1027px]:block flex-1 text-center">GitHub</p>
                 </a>
-                <a href="https://moiz.lu" target="_blank" title="リンク集" class="p-2 rounded-full bg-base/25 backdrop-blur-2xl pointer-events-auto">
+                <a href="https://moiz.lu" target="_blank" title="リンク集" class="p-2 flex-1 rounded-full bg-base/25 backdrop-blur-2xl border flex justify-start items-center gap-2 pointer-events-auto">
                     <SvgIcon Svg={LinkIcon} size={30} />
+                    <p class="hidden min-[736px]:block flex-1 text-center">各種リンクなど</p>
                 </a>
-
             </div>
 
-            <div class="sm:w-70 flex-center overflow-clip gap-6 p-1.5 rounded-full bg-base/25 backdrop-blur-sm pointer-events-auto border-label border">
-                <a href={`#${nextSection}`} class={["flex-1 transition-all duration-300", (isFirstSection) && "-translate-y-10 opacity-0 pointer-events-none"]}>
-                    <div class="flex justify-end items-center gap-2">
-                        <!-- {#each section }
+            <div class="fixed w-full flex-center">
+                <div class="h-13 sm:w-70 flex-center overflow-clip gap-6 p-1.5 rounded-full bg-base/25 backdrop-blur-sm pointer-events-auto border-label border">
+                    <a href={`#${nextSection}`} class={["flex-1 transition-all duration-300", (isFirstSection) && "-translate-y-10 opacity-0 pointer-events-none"]}>
+                        <div class="flex justify-end items-center gap-2">
+                            <!-- {#each section }
 
-                        {/each} -->
-                        <p class="hidden sm:block">{`${nextSection.slice(0, 1).toUpperCase()}${nextSection.slice(1)}`}</p>
-                        <SvgIcon Svg={ArrowIcon} size={40} class="" />
-                    </div>
-                </a>
-                <a href={`#${prevSection}`} class={["flex-1 transition-all duration-300", (isLastSection) && "translate-y-10 opacity-0 pointer-events-none"]}>
-                    <div class="flex justify-start items-center gap-2">
-                        <SvgIcon Svg={ArrowIcon} size={40} class="rotate-180" />
-                        <p class="hidden sm:block">{`${prevSection.slice(0, 1).toUpperCase()}${prevSection.slice(1)}`}</p>
-                    </div>
-                </a>
+                            {/each} -->
+                            <p class="hidden sm:block">{`${nextSection.slice(0, 1).toUpperCase()}${nextSection.slice(1)}`}</p>
+                            <SvgIcon Svg={ArrowIcon} size={40} class="" />
+                        </div>
+                    </a>
+                    <a href={`#${prevSection}`} class={["flex-1 transition-all duration-300", (isLastSection) && "translate-y-10 opacity-0 pointer-events-none"]}>
+                        <div class="flex justify-start items-center gap-2">
+                            <SvgIcon Svg={ArrowIcon} size={40} class="rotate-180" />
+                            <p class="hidden sm:block">{`${prevSection.slice(0, 1).toUpperCase()}${prevSection.slice(1)}`}</p>
+                        </div>
+                    </a>
+                </div>
             </div>
 
             <div class="flex-center p-1.5 rounded-full bg-base/25 backdrop-blur-sm pointer-events-auto">
