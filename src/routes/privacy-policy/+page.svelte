3e210dfceb71aa9fa9c7668fccc24d21d45f9fc0
もいずる <momizibe@outlook.jp>
2026-05-31 14:09:33 +0900

    Update +page.svelte

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -16,20 +16,22 @@
     import En from "$lib/docs/privacy-policy/en.md";
 </script>
 
-<main class="document py-15 w-full max-w-200 mx-auto px-4 prose xl:prose-xl
-             prose-p:text-label prose-h1:text-label prose-h1:text-3xl prose-h2:text-label prose-h3:text-label prose-li:text-label prose-li:marker:text-label prose-ul:text-label prose-strong:text-label prose-table:text-label prose-th:text-label prose-a:text-label">
+<main class="document py-15 w-full max-w-200 mx-auto px-4 ">
     <Header displaysNav={false} tapAvatarToReload={true} tapAvatarToSection="#contact" />
 
-    <button onclick={() => history.back()} class="z-1000 fixed -top-3 left-30 cursor-pointer flex justify-center items-center">
-        <SvgIcon Svg={ArrowStickIcon} size={50} class="text-label -rotate-90" />
-        <p>{m.back()}</p>
+    <button onclick={() => history.back()} class="z-1000 h-13 fixed bottom-2 left-2 p-2 cursor-pointer rounded-sm bg-label flex justify-center items-center text-base">
+        <SvgIcon Svg={ArrowStickIcon} size={40} class="-rotate-90" />
+        <p class="text-xl">{m.back()}</p>
     </button>
 
-    {#if getLocale() === "ja"}
-        <Jp />
-    {:else}
-        <En />
-    {/if}
+    <div class="w-full mx-auto prose xl:prose-xl
+              prose-p:text-label prose-h1:text-label prose-h1:text-3xl prose-h2:text-label prose-h3:text-label prose-li:text-label prose-li:marker:text-label prose-ul:text-label prose-strong:text-label prose-table:text-label prose-th:text-label prose-a:text-label">
+        {#if getLocale() === "ja"}
+            <Jp />
+        {:else}
+            <En />
+        {/if}
+    </div>
 </main>
 
 <style>
