4d3bf0d5e6df1748a051a785efed13e9ae842241
もいずる <momizibe@outlook.jp>
2026-05-31 14:35:14 +0900

    Update +page.svelte

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -5,6 +5,8 @@
 <script lang="ts">
     import ArrowStickIcon from "$lib/assets/icons/arrow-stick.svelte";
 
+    import { browser } from "$app/environment";
+
     import { getLocale } from "$lib/paraglide/runtime";
     import { m } from "$lib/paraglide/messages";
 
@@ -19,10 +21,12 @@
 <main class="document py-15 w-full max-w-200 mx-auto px-4 ">
     <Header displaysNav={false} tapAvatarToReload={true} tapAvatarToSection="#contact" />
 
-    <button onclick={() => history.back()} class="z-1000 h-13 fixed bottom-2 left-2 p-2 cursor-pointer rounded-sm bg-label flex justify-center items-center text-base">
-        <SvgIcon Svg={ArrowStickIcon} size={40} class="-rotate-90" />
-        <p class="text-xl">{m.back()}</p>
-    </button>
+    {#if browser && document.referrer && document.referrer.startsWith(window.location.origin)}
+        <button onclick={() => history.back()} class="z-1000 h-13 fixed bottom-2 left-2 p-2 cursor-pointer rounded-sm bg-label flex justify-center items-center text-base">
+            <SvgIcon Svg={ArrowStickIcon} size={40} class="-rotate-90" />
+            <p class="text-xl">{m.back()}</p>
+        </button>
+    {/if}
 
     <div class="w-full mx-auto prose xl:prose-xl
               prose-p:text-label prose-h1:text-label prose-h1:text-3xl prose-h2:text-label prose-h3:text-label prose-li:text-label prose-li:marker:text-label prose-ul:text-label prose-strong:text-label prose-table:text-label prose-th:text-label prose-a:text-label">
